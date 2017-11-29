f1807f16348e61ac8cc0d5ebe70d0b043d5acd0f
Jose Caballero <jcaballero@bnl.gov>
2017-11-29 15:27:42 -0500

    adding module with exceptions

---
+++ novissima/novissimaex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# =============================================================================
+#
+#           Exception classes for novissima
+#
+# =============================================================================
+
+
+
+class ConfigurationFailure(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return self.value
+
+
+class TimeOut(Exception):
+    def __init__(self, task, timout):
+        self.value = 'task %s timed out after %s seconds.' %(task, timeout)
+    def __str__(self):
+        return self.value
+
